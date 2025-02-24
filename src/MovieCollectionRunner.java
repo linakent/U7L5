8ce01afc774aeeaf319c9f9241a5097e3ed20da3
BT_2S14_03 <linak9@nycstudents.net>
2025-02-24 10:59:42 -0500

    Did start w'rking on task 4 and did finish the previous tasks

---
+++ src/MovieCollectionRunner.java
@@ -0,0 +1,8 @@
+public class MovieCollectionRunner
+{
+    public static void main(String arg[])
+    {
+        MovieCollection myCollection = new MovieCollection("src/movies_data.csv");
+        myCollection.menu();
+    }
+}
